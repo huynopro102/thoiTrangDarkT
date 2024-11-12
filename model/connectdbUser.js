c23bdcd1d828a6548c3c5ba5a8af87a99fcb47d1
x thang <x_thang@example.com>
2024-11-12 10:06:42 +0700

    fix apiController forgot password

--- model/connectdbUser.js
+++ model/connectdbUser.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise")
 
 
 const poolUser = mysql.createPool({
-  port: '3306',
-  host: '13.215.153.56',
-  user: 'root',
-  password: 'root',
-  database: 'nodejsbasic'
+  port: process.env.PORT,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
   });
   
 
